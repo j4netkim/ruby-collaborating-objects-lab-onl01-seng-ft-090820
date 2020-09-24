00858f73bb8b1b1e896fe1c572cd2281ad7402a7
J K <kj688895@gmail.com>
2020-09-24 22:11:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,16 +17,5 @@ class Artist
     Song.all.select do |song|
       song.artist == self
     end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name)
-  end
-
-  def print_songs
-    @songs.each {|song| puts song.name}
   end
 end
