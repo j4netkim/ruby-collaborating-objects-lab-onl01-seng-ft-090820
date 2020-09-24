b844f2c33e55207ac648474c2e1ed6b929a73601
J K <kj688895@gmail.com>
2020-09-24 21:47:26 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(filepath)
+    @path = filepath
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+  end
+
+  def import
+    
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name
+
+
+end
