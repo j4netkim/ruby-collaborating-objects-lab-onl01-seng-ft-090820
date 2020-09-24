73c2b98f31d9f0f04af2f44c65688dfe0f951554
J K <kj688895@gmail.com>
2020-09-24 22:23:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,4 +18,12 @@ class Song
     @@all
   end 
 
+  def self.new_by_filename(filename)
+    songarray = filename.split("-")
+    name = songarray[1]
+    song = self.new(name)
+    song.artist_name = songarray[0]
+    song 
+  end 
+
 end
