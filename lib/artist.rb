568ecdd3e530fecf5db429bed4d7c04180a579a3
J K <kj688895@gmail.com>
2020-09-24 22:15:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,9 @@ class Artist
     self.find_by_name(name) || self.create_by_name(name)
   end
 
-  
+  def self.find_by_name(name)
+    self.all.find {|artist| artist.name == name}
+  end 
 
 
 end
